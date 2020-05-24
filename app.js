c19a61ec1572e26d633ca1b02692c4b0dbf3b453
Vinita <vinitavkumbhar@gmail.com>
2020-05-24 18:43:05 +0530

    error handling added 1

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const AppError = require("./utils/appErrors");
+const globalErrorHandler = require("./controllers/errorController");
 
 const toDoRouter = require("./routes/todoRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -11,6 +13,12 @@ app.use(express.json());
 
 app.use("/api/v1/todo", toDoRouter);
 app.use("/api/v1/user", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server! 👻`, 404));
+});
+
+app.use(globalErrorHandler);
 
 let DB = process.env.DB;
 mongoose
