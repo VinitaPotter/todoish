d6b7478bd76df867ed3a73ffa1c995d1cec041ca
Vinita <vinitavkumbhar@gmail.com>
2020-05-26 09:56:30 +0530

    error handling update

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
 
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("Uncaught exception!! Shutting now..");
+  process.exit(1);
+});
+
 let server = app.listen(parseInt(process.env.PORT), () => {
   console.log(`App running on port ${process.env.PORT}`);
 });
@@ -10,14 +16,6 @@ let server = app.listen(parseInt(process.env.PORT), () => {
 process.on("unhandledRejection", (err) => {
   console.log(err.name, err.message);
   console.log("Unhandled Rejection!! Shutting now..");
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-process.on("uncaughtException", (err) => {
-  console.log(err.name, err.message);
-  console.log("Uncaught exception!! Shutting now..");
   server.close(() => {
     process.exit(1);
   });
