f9a423556c6f4e5769616d1c74e14978ec23f95f
Vinita <vinitavkumbhar@gmail.com>
2020-05-26 09:54:36 +0530

    error handling 2

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,56 @@
+const AppError = require("../utils/appErrors");
+
+const handleDBCastError = (err) => {
+  let message = `Invalid ${err.path} : ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDBDuplicateFields = (err) => {
+  let value = err.errmsg.match(/(["'])(\\?.)*?\1/);
+  let message = `Duplicate field value: ${value[0]}`;
+  return new AppError(message, 400);
+};
+
+const handleDBValidationError = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  let message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
+const devError = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+const prodError = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error("ERROR 🎆", err);
+    res.status(500).json({
+      status: "Error",
+      message: "Something went wrong!",
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "Error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV == "development") {
+    devError(err, res);
+  } else if (process.env.NODE_ENV == "prouction") {
+    let error = { ...err };
+    if (error.name == "CastError") error = handleDBCastError(error);
+    if (error.code == 11000) error = handleDBDuplicateFields(error);
+    if (error.name == "ValidationError") error = handleDBValidationError(error);
+    prodError(error, res);
+  }
 };
--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
 
-app.listen(parseInt(process.env.PORT), () => {
+let server = app.listen(parseInt(process.env.PORT), () => {
   console.log(`App running on port ${process.env.PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("Unhandled Rejection!! Shutting now..");
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("Uncaught exception!! Shutting now..");
+  server.close(() => {
+    process.exit(1);
+  });
 });
