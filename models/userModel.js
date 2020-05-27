c08b00a6ced6dbbf14615c0d465fccce7e9d75e2
Vinita <vinitavkumbhar@gmail.com>
2020-05-27 09:36:02 +0530

    User sign up and password added

--- models/userModel.js
+++ models/userModel.js
@@ -1,23 +1,49 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userScheme = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
+const userScheme = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please type a password"],
+      minlength: 8,
+    },
+    confirmPassword: {
+      type: String,
+      required: [true, "Please confirm your password"],
+      minlength: 8,
+      validate: {
+        validator: function (current_password) {
+          return current_password === this.password;
+        },
+        message: "Passwords are not the same",
+      },
+    },
   },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    lowercase: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    timestamps: { createdAt: "created_at" },
+  }
+);
+
+userScheme.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword = undefined;
+  next();
 });
 
 const User = new mongoose.model("User", userScheme);
--- server.js
+++ server.js
@@ -3,20 +3,20 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
 
-process.on("uncaughtException", (err) => {
-  console.log(err.name, err.message);
-  console.log("Uncaught exception!! Shutting now..");
-  process.exit(1);
-});
+// process.on("uncaughtException", (err) => {
+//   console.log(err.name, err.message);
+//   console.log("Uncaught exception!! Shutting now..");
+//   process.exit(1);
+// });
 
-let server = app.listen(parseInt(process.env.PORT), () => {
+app.listen(parseInt(process.env.PORT), () => {
   console.log(`App running on port ${process.env.PORT}`);
 });
 
-process.on("unhandledRejection", (err) => {
-  console.log(err.name, err.message);
-  console.log("Unhandled Rejection!! Shutting now..");
-  server.close(() => {
-    process.exit(1);
-  });
-});
+// process.on("unhandledRejection", (err) => {
+//   console.log(err.name, err.message);
+//   console.log("Unhandled Rejection!! Shutting now..");
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
